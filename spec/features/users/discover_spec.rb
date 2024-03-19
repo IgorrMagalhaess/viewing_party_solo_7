67cf8210068e51b0232ee5fd7797075414dcfd94
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-03-19 15:07:30 -0400

    test discover page

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -2,8 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'User discover Pages', type: :feature do
    describe 'As a user' do
-      it 'has different options to discover new movies' do
+      before(:each) do
+         @user = User.create!(name: 'Tommy', email: 'tommy@email.com')
+   
          visit user_discover_path
+      end
+
+      it 'has a button to discover top rated movies' do
+         expect(page).to have_button('Top Rated Movies')
+      end
+
+      it 'has a text field to search for a movie title' do
+         expect(page).to have_content('Search by Title')
+         expect(page).to have_field('Search')
+      end
+
+      it 'has a button to search for a movie title' do
+         expect(page).to have_button('Search')
       end
    end
 end
