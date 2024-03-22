2264dc5bb98a6dff6c1de71b6228c3e0519c8157
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-03-22 09:07:49 -0400

    refactoring facade to include memoization and limit amount of hit to API

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -1,51 +1,46 @@
 class MoviesFacade
-   attr_reader :params
-   
-   def initialize(params)
+   def initialize(params, service = MoviesService.new)
       @params = params
+      @service = service
+      @movies = nil
+      @movie = nil
+      @movie_cast = nil
+      @movie_reviews = nil
+      @watch = nil
+      @similar = nil
    end
 
    def movies
-      service = MoviesService.new
-      if @params.nil?
-         json = service.get_top_rated_movies
-      else
-         json = service.get_movies_by_title(@params)
-      end
-
-      @movies = json[:results].map do |movie_data|
-         Movie.new(movie_data)
+      @movies ||= begin
+         json = @params.nil? ? @service.get_top_rated_movies : @service.get_movies_by_title(@params)
+         @movies = json[:results].map { |movie_data| Movie.new(movie_data) }
       end
    end
 
    def movie_by_id
-      service = MoviesService.new
-      movie_json = service.get_movie_by_id(@params)
-      @movie = Movie.new(movie_json)
+      @movie ||= begin
+         movie_json = @service.get_movie_by_id(@params)
+         @movie = Movie.new(movie_json)
+      end
    end
 
    def cast_by_movie_id
-      service = MoviesService.new
-      movie_cast_json = service.get_movie_cast_by_id(@params)
-
-      cast = movie_cast_json[:cast].map do |movie_cast|
-         movie_cast[:name]
+      @movie_cast ||= begin
+         movie_cast_json = @service.get_movie_cast_by_id(@params)
+         cast = movie_cast_json[:cast].map { |movie_cast| movie_cast[:name] }.take(10)
+         @movie.cast = cast
+         cast
       end
-      @movie.cast = cast.take(10)
-      @movie_cast = @movie.cast
    end
 
    def reviews_by_movie_id
-      service = MoviesService.new
-      reviews_json = service.get_movie_reviews_by_id(@params)
-
-      reviews = Hash.new()
-      reviews_json[:results].map do |review|
-         reviews[review[:author]] = review[:content]
+      @movie_reviews ||= begin
+         reviews_json = @service.get_movie_reviews_by_id(@params)
+         reviews = Hash.new()
+         reviews_json[:results].map { |review| reviews[review[:author]] = review[:content] }.take(10)
+         @movie.reviews = reviews
+         reviews
       end
-      
-      @movie.reviews = reviews.take(10)
-      @movie_reviews = @movie.reviews
    end
 
    def convert_min_to_hours(min)
@@ -55,21 +50,24 @@ class MoviesFacade
    end
 
    def where_to_watch
-      service = MoviesService.new
-      where_to_watch_json = service.get_where_to_watch(@params)
-
-      buy = where_to_watch_json[:results][:US][:buy]
-      rent = where_to_watch_json[:results][:US][:rent]
-
-      @watch = [buy, rent]
+      @watch ||= begin
+         where_to_watch_json = @service.get_where_to_watch(@params)
+         buy = where_to_watch_json[:results][:US][:buy]
+         rent = where_to_watch_json[:results][:US][:rent]
+         [buy, rent]
+      end
    end
 
    def similar_movies
-      service = MoviesService.new
-      similar_movies_json = service.get_similar_movies(@params)
-
-      @movies = similar_movies_json[:results].map do |movie_data|
-         Movie.new(movie_data)
+      @similar ||= begin
+         similar_movies_json = @service.get_similar_movies(@params)
+         similar_movies_json[:results].map { |movie_data| Movie.new(movie_data) }
       end
+   end
+
+   private
+
+   def params
+      @params 
    end
 end
--- spec/facades/movies_facade_spec.rb
+++ spec/facades/movies_facade_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe MoviesFacade do
          facade = MoviesFacade.new("Star Wars")
 
          expect(facade).to be_a MoviesFacade
-         expect(facade.params).to eq("Star Wars")
       end
    end
 
@@ -21,6 +20,33 @@ RSpec.describe MoviesFacade do
          expect(movie.review_count).to be_a Integer
          expect(movie.avg_rating).to be_a Float
          expect(movies).to be_a Array
+      end
+   end
+
+   describe '#movie_by_id' do
+      it 'creates a Movie Object', :vcr do
+         facade = MoviesFacade.new(550)
+         movie = facade.movie_by_id
+
+         expect(movie).to be_a Movie
+         expect(movie.title).to be_a String
+         expect(movie.review_count).to be_a Integer
+         expect(movie.avg_rating).to be_a Float
+         expect(movie.runtime).to be_a Integer
+         expect(movie.genre).to be_a Array
+         expect(movie.cast).to be_a Array
+         expect(movie.reviews).to be_a Hash
+         expect(movie.release_date).to be_a String
+      end
+   end
+
+   describe "#cast_by_movie_id" do
+      it 'returns the cast for a given movie', :vcr do
+         facade = MoviesFacade.new(550)
+         movie = facade.movie_by_id
+         cast = facade.cast_by_movie_id
+
+
       end
    end
 end
