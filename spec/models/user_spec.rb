e40e34f453b3adba2785d368db89398e3928dc2f
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-03-22 12:24:08 -0400

    test: include missing model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,4 +17,33 @@ RSpec.describe User, type: :model do
     it { should have_many(:viewing_parties).through(:user_parties) }
   end
   
+  describe '#invited_to_viewing_parties' do
+    it 'returns viewing parties user is invited to' do
+      user = User.create!(name: 'John Doe', email: 'john@example.com')
+      hosted_party = ViewingParty.create!(duration: 120, date: Date.today + 3.days, start_time: '18:00', movie_id: 1)
+      invited_party = ViewingParty.create!(duration: 90, date: Date.today + 5.days, start_time: '20:00', movie_id: 2)
+      UserParty.create!(user: user, viewing_party: hosted_party, host: true)
+      UserParty.create!(user: user, viewing_party: invited_party, host: false)
+
+      invited_viewing_parties = user.invited_to_viewing_parties
+
+      expect(invited_viewing_parties).to include(invited_party)
+      expect(invited_viewing_parties).not_to include(hosted_party)
+    end
+  end
+
+  describe '#hosting_viewing_parties' do
+    it 'returns viewing parties user is hosting' do
+      user = User.create!(name: 'Jane Doe', email: 'jane@example.com')
+      hosted_party = ViewingParty.create!(duration: 120, date: Date.today + 3.days, start_time: '18:00', movie_id: 1)
+      invited_party = ViewingParty.create!(duration: 90, date: Date.today + 5.days, start_time: '20:00', movie_id: 2)
+      UserParty.create!(user: user, viewing_party: hosted_party, host: true)
+      UserParty.create!(user: user, viewing_party: invited_party, host: false)
+
+      hosting_viewing_parties = user.hosting_viewing_parties
+
+      expect(hosting_viewing_parties).to include(hosted_party)
+      expect(hosting_viewing_parties).not_to include(invited_party)
+    end
+  end
 end
